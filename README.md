d70874e91b4799d3d7e06c2eca89f1973bbc52fd
Zach Miller <zachmiller@ucsb.edu>
2026-05-03 02:10:13 -0700

    Added relevant playlist ids

---
+++ README.md
@@ -0,0 +1,9 @@
+Queries searched:
+"UCSB",
+"UC Santa Barbara",
+"University of California, Santa Barbara",
+"University of California Santa Barbara",
+"Isla Vista",
+"Deltopia"
+
+The final list has been manually filtered (cut by ~250 playlists).
---
+++ playlist_search.py
@@ -0,0 +1,51 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from typing import Any
+
+QUERIES = [
+    "UCSB",
+    "UC Santa Barbara",
+    "University of California, Santa Barbara",
+    "University of California Santa Barbara",
+    "Isla Vista",
+    "Deltopia"
+]
+
+auth_manager = SpotifyClientCredentials(
+    client_id="068d3dfa31d0431391104cddbc252aed",
+    client_secret="10a2355c48f34fc2934d17e45aa5a20b"
+)
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+def get_playlists(query: str) -> list[str]:
+    playlists = set()
+
+    for i in range(0, 1001, 10):
+        response = sp.search(
+            q = query,
+            type = "playlist",
+            limit = 10,
+            offset=i
+        )
+
+        if response["playlists"]["next"] is None:
+            break
+        
+
+        for item in response["playlists"]["items"]:
+            if item is None:
+                break
+            
+            playlists.add(item["id"])
+    
+    return playlists
+
+if __name__ == "__main__":
+    all_playlists = set()
+
+    for query in QUERIES:
+        all_playlists = all_playlists.union(get_playlists(query))
+
+    with open("playlist_ids.txt", "w") as f:
+        for playlist_id in all_playlists:
+            f.write(playlist_id+'\n')
